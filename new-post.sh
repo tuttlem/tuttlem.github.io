1badeb64a45df1ace5af19ddc5bb8a7ea81cb63f
Michael Tuttle <tuttlem@gmail.com>
2015-10-06 13:10:48 +1000

    Create new post script

---
+++ new-post.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ----------------------------------------------------
+# Template blogpost setup script
+#
+# Creates a new template markdown file for the cogs
+# and levers blog and opens the file automatically in
+# emacs.
+# ---------------------------------------------------
+
+DATE_SLUG=$(date +%Y-%m-%d)
+TITLE=$(echo "$@" | tr -s '[:space:]' '\n' | tr -s '[:upper:]' '[:lower:]' | paste -sd-)
+FNAME=_posts/$DATE_SLUG-$TITLE.md
+
+cat >$FNAME <<EOL
+---
+layout: post
+title: $@
+date: $DATE_SLUG
+comments: false
+categories: [ "" ]
+---
+
+EOL
+
+emacs $FNAME
+
