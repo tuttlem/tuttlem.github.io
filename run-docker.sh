e623a46497372d5c0f3ea8da35800310fb09512d
Michael Tuttle <tuttlem@gmail.com>
2016-02-10 22:40:51 +1000

    Minor config update and change of container

--- run-docker.sh
+++ run-docker.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#
-# This script will use the default jekyll:jekyll docker image to
-# host this blog in a re-load state
-
-docker run --rm -v $(pwd):/srv/jekyll -p 4000:4000 -e MENTOS_TIMEOUT=500000 jekyll/jekyll 
+docker run --rm --interactive -v $(pwd):/src -p 4000:4000 -e MENTOS_TIMEOUT=500000 grahamc/jekyll serve --host 0.0.0.0 --watch 
