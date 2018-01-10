a3fc68815865595d6e09cd845ef779eba46e7447
Michael Tuttle <tuttlem@gmail.com>
2018-01-10 21:48:00 +1000

    New article

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --interactive -v $(pwd):/src -p 4000:4000 -e MENTOS_TIMEOUT=500000 grahamc/jekyll serve --host 0.0.0.0 --watch 
+docker run --rm --interactive -v $(pwd):/srv/jekyll -p 4000:4000 -e MENTOS_TIMEOUT=500000 jekyll/jekyll:pages jekyll serve --host 0.0.0.0 --watch --incremental
